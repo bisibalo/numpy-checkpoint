3abef7f3108c5dadfb566dae6e7f8d191abfb538
bisibalo <bisibalogun104@yahoo.com>
2025-01-08 20:53:45 -0800

    GIT SYNTAX

---
+++ dangerous_in_python.py
@@ -0,0 +1,14 @@
+# GIT ADD INITIALIZATION AND ALL SYNTAX
+# C:\Users\HP\Desktop\gomycode>cd..   TO EXIT DIRECTORY
+# C:\Users\HP\Desktop>cd gomycoden TO GET BACK IN DIRECTORY
+# git init
+# git add " file name"
+# git remote add origin (link from repository)
+# git commit -m ""
+
+#TO CONFIG
+#C:\Users\HP\Desktop\gomycode>git config --global user.email "bisibalogun104@yahoo.com"
+#C:\Users\HP\Desktop\gomycode>git config --global user.name "bisibalo"
+
+
+# git push -u origin master
